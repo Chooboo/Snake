63ee4685277da78bada333a5f36b9ad1a65f32c1
Jakub <kubo.suriak@gmail.com>
2021-02-06 15:45:01 +0000

    customized

--- server.py
+++ server.py
@@ -77,10 +77,10 @@ class Battlesnake(object):
         # TIP: If you open your Battlesnake URL in browser you should see this data
         return {
             "apiversion": "1",
-            "author": "",  # TODO: Your Battlesnake Username
-            "color": "#666666",  # TODO: Personalize
-            "head": "default",  # TODO: Personalize
-            "tail": "default",  # TODO: Personalize
+            "author": "Chooboo",  # TODO: Your Battlesnake Username
+            "color": "#800000",  # TODO: Personalize
+            "head": "caffeine",  # TODO: Personalize
+            "tail": "skinny",  # TODO: Personalize
         }
 
     @cherrypy.expose
